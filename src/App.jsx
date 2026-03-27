42610c1ad05b68c1bc4993f158d3f79503a7a700
kiran dwivedi <snehatiwari7654321@gmail.com>
2026-03-27 22:37:19 +0530

    file

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom" ;
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer" ;
 import Admin from "./Components/Admin";
